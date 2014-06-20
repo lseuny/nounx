4fd055f15a795e608c67bf1eef5715fde6cc171f
lseuny <lseuny@gmail.com>
2014-06-21 00:59:36 +0900

    .

--- nounx.py
+++ nounx.py
@@ -5,11 +5,12 @@ import re
 import math
 
 
-# Minimum DF(Document Frequency) for noun candidates
-MIN_DF = 6
+# Minimum occurrence of DF(Document Frequency) of words to be a noun candidate
+MIN_DF = 30
 
 
-ws_ptn = re.compile(u'[\s"]+')
+#ws_ptn = re.compile(u'[\s"''.,;:/!?@()[\]]+')
+ws_ptn = re.compile(u'[^a-zA-Z0-9_ㄱ-ㅎ가-힣]+')
 
 
 def load_dic(path='./dic.txt'):
@@ -18,8 +19,8 @@ def load_dic(path='./dic.txt'):
     for line in f:
         lst = line.strip().split('\t')
         if len(lst) != 2: continue
-        if float(lst[1]) < 0: continue
-        dic[lst[0]] = lst[1] ### if duplicated??
+        #if float(lst[1]) < 0: continue
+        dic[lst[0]] = float(lst[1]) ### if duplicated??
     f.close()
     return dic
 
@@ -45,42 +46,61 @@ def make_postfix_ptn(postfix_list):
 
 def extract_noun(text, dic_noun, postfix_ptn):
     noun_list = []
-    ws_ptn = re.compile(u'[\s]+')
     u_text = ws_ptn.sub(u' ', text.lower().decode('utf8', 'ignore'))
     for word in u_text.split(u' '):
+        if word.endswith(u'들'):
+            word = word[:-1]
         noun = word.encode('utf8', 'ignore')
-        if noun in dic_noun:
+        if noun in dic_noun and dic_noun[noun] > 0.1:
             noun_list.append(noun)
             continue
 
         m = postfix_ptn.search(word)
         if m == None or m.start() < 2: continue
-        noun = word[:m.start()].encode('utf8', 'ignore')
-        if noun in dic_noun:
+        word = word[:m.start()]
+        if word.endswith(u'들'):
+            word = word[:-1]
+        elif word.endswith(u'에서'):
+            word = word[:-2]
+        noun = word.encode('utf8', 'ignore')
+        if noun in dic_noun and dic_noun[noun] > 0.1:
             noun_list.append(noun)
     return noun_list
 
 
 def get_noun_candidate(filepath, postfix_list, min_df=MIN_DF):
     postfix_ptn = make_postfix_ptn(postfix_list)
+    digit_ptn = re.compile(u'[0-9]+')
 
-    noun_freq = {}
+    noun_df = {}
     f = open(filepath, 'rt')
     for line in f:
         u_str = ws_ptn.sub(u' ', line.lower().decode('utf8', 'ignore'))
+        noun_tf = {}
         for word in u_str.split(u' '):
             m = postfix_ptn.search(word)
             if m == None or m.start() < 1: continue
-            noun = word[:m.start()].encode('utf8', 'ignore')
-            if noun not in noun_freq:
-                noun_freq[noun]  = 1
-            else:
-                noun_freq[noun] += 1
+            word = word[:m.start()]
+            m = digit_ptn.search(word)
+            if m != None and m.start() >= 0: continue
+            if word.endswith(u'들'):
+                word = word[:-1]
+            elif word.endswith(u'에서'):
+                word = word[:-2]
+            noun = word.encode('utf8', 'ignore')
+            if noun not in noun_tf:
+                noun_tf[noun] = 0
+            noun_tf[noun] += 1
+        for n in noun_tf.keys():
+            if n not in noun_df:
+                noun_df[n] = 0
+            noun_df[n] += 1
     f.close()
 
     noun_list = []
-    for n, f in noun_freq.items():
-        if f < min_df: continue
+    for n, df in noun_df.items():
+        if df < min_df:
+            continue
         noun_list.append(n)
     return noun_list
 
@@ -96,12 +116,15 @@ def get_noun_postfix_dist(filepath, noun_list, postfix_list):
         for word in u_str.split(u' '):
             m = postfix_ptn.search(word)
             if m == None or m.start() < 2: continue
-            noun = word[:m.start()].encode('utf8', 'ignore')
+            u_noun = word[:m.start()]
+            if u_noun.endswith(u'들'):
+                u_noun = u_noun[:-1]
+            elif u_noun.endswith(u'에서'):
+                u_noun = u_noun[:-2]
+            noun = u_noun.encode('utf8', 'ignore')
             postfix = word[m.start():].encode('utf8', 'ignore')
             if noun not in noun_list: continue
-            if postfix not in postfix_list:
-                print '\t\t\t%s' % postfix
-                continue
+            if postfix not in postfix_list: continue
             if noun not in noun_postfix:
                 noun_postfix[noun] = [0] * len(postfix_list)
             noun_postfix[noun][postfix_list.index(postfix)] += 1
